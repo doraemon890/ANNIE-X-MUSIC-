f0ea5d743ac63eb187a32aeaa25f8a109eaeb141
RAJNISH XD <155803358+doraemon890@users.noreply.github.com>
2024-02-23 23:08:09 +0530

    Update cp.py

--- ANNIEMUSIC/plugins/tools/cp.py
+++ ANNIEMUSIC/plugins/tools/cp.py
@@ -1,36 +1,20 @@
 from ANNIEMUSIC import ANNIEMUSIC as app
 from pyrogram import Client, filters
 
-async def delete_links(message):
+async def delete_messages(message):
     if any(link in message.text for link in ["http", "https", "www."]):
         await message.delete()
-
-@app.on_message(filters.group & filters.text & ~filters.me)
-async def handle_messages(client, message):
-    await delete_links(message)
-
-@app.on_edited_message(filters.group & filters.text & ~filters.me)
-async def handle_edited_messages(client, edited_message):
-    await delete_links(edited_message)
+    elif any(keyword in message.text for keyword in keywords_to_delete) and len(message.text.split()) > 20:
+        await message.delete()
+    elif len(message.text.split()) >= 20:
+        await message.delete()
 
 keywords_to_delete = ["NCERT", "XII", "page", "Ans", "meiotic", "divisions", "System.in", "Scanner", "void", "nextInt"]
 
 @app.on_message(filters.group & filters.text & ~filters.me)
-async def delete_specific_keywords(client, message):
-    if any(keyword in message.text for keyword in keywords_to_delete) and len(message.text.split()) > 20:
-        await message.delete()
+async def handle_messages(client, message):
+    await delete_messages(message)
 
 @app.on_edited_message(filters.group & filters.text & ~filters.me)
-async def delete_edited_specific_keywords(client, edited_message):
-    if any(keyword in edited_message.text for keyword in keywords_to_delete) and len(edited_message.text.split()) > 20:
-        await edited_message.delete()
-
-@app.on_message(filters.group & filters.text & ~filters.me)
-async def delete_long_messages(client, message):
-    if len(message.text.split()) >= 20:
-        await message.delete()
-
-@app.on_edited_message(filters.group & filters.text & ~filters.me)
-async def delete_edited_long_messages(client, edited_message):
-    if len(edited_message.text.split()) >= 20:
-        await edited_message.delete()
+async def handle_edited_messages(client, edited_message):
+    await delete_messages(edited_message)
