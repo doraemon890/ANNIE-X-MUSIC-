f83e88a354c55f58c5cf3ac6d910fda3878137b5
RAJNISH XD <155803358+doraemon890@users.noreply.github.com>
2024-03-02 20:34:50 +0530

    Delete cele.py

--- cele.py
+++
@@ -1,8 +0,0 @@
-import re
-from os import getenv
-
-from dotenv import load_dotenv
-from pyrogram import filters
-
-CELEBRATION_VID_URL = getenv(
-    "CELEBRATION_VID_URL", "https://github.com/doraemon890/ANNIE-X-MUSIC-/assets/155803358/4671acf4-d356-4698-baad-43ef62438437" )
