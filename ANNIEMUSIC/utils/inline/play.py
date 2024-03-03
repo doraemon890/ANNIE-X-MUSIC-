070530ba590a19591b687829a39f12f46fa06c43
RAJNISH XD <155803358+doraemon890@users.noreply.github.com>
2024-03-03 21:26:55 +0530

    Update play.py

--- ANNIEMUSIC/utils/inline/play.py
+++ ANNIEMUSIC/utils/inline/play.py
@@ -45,27 +45,33 @@ def stream_markup_timer(_, videoid, chat_id, played, dur):
     duration_sec = time_to_seconds(dur)
     percentage = (played_sec / duration_sec) * 100
     umm = math.floor(percentage)
-    if 0 < umm <= 50:
+    if 0 < umm <= 10:
         bar = "◉—————————"
-    elif 50 < umm < 60:
+    elif 10 < umm < 20:
         bar = "—◉————————"
-    elif 60 <= umm < 70:
+    elif 20 <= umm < 30:
         bar = "——◉———————"
-    elif 70 <= umm < 80:
+    elif 30 <= umm < 40:
         bar = "———◉——————"
-    elif 80 <= umm < 95:
+    elif 40 <= umm < 50:
+        bar = "————◉—————"
+    elif 50 <= umm < 60:
+        bar = "—————◉————"
+    elif 60 <= umm < 70:
         bar = "——————◉———"
+    elif 70 <= umm < 80:
+        bar = "———————◉——"
+    elif 80 <= umm < 95:
+        bar = "————————◉—"
     else:
         bar = "—————————◉"
-        
-    buttons  = [
-
-        [
+    buttons = [
+                [
             InlineKeyboardButton(
-                text=_["S_B_5"],
-                url=f"https://t.me/{app.username}?startgroup=true",
+                text=f"{played} {bar} {dur}",
+                callback_data="GetTimer",
             ),
-          ],
+        ],
           [
             InlineKeyboardButton(
                 text="II ᴘᴀᴜsᴇ",
