80c7afd77351c1764ccceaaf989b6304d36cae69
RAJNISH XD <155803358+doraemon890@users.noreply.github.com>
2024-03-04 18:34:28 +0530

    Update botschk.py

--- ANNIEMUSIC/plugins/sudo/botschk.py
+++ ANNIEMUSIC/plugins/sudo/botschk.py
@@ -5,10 +5,8 @@ from config import OWNER_ID
 
 BOT_LIST = ["Annie_X_music_bot"]
 
-
-
 @app.on_message(filters.command("botschk") & filters.user(OWNER_ID))
-async def bots_chk(app, message):
+async def bots_chk(_, message):
     msg = await message.reply_photo(photo="https://telegra.ph/file/e7a1c0481617facf5fb37.jpg", caption="**ᴄʜᴇᴄᴋɪɴɢ ʙᴏᴛs sᴛᴀᴛs ᴀʟɪᴠᴇ ᴏʀ ᴅᴇᴀᴅ...**")
     response = "**ʙᴏᴛs sᴛᴀᴛᴜs ᴅᴇᴀᴅ ᴏʀ ᴀʟɪᴠᴇ ᴄʜᴇᴄᴋᴇʀ**\n\n"
     for bot_username in BOT_LIST:
@@ -20,51 +18,41 @@ async def bots_chk(app, message):
             await asyncio.sleep(3)
             async for bot_message in userbot.get_chat_history(bot_id, limit=1):
                 if bot_message.from_user.id == bot_id:
-                    response += f"╭⎋ [{bot.first_name}](tg://user?id={bot.id})\n╰⊚ **sᴛᴀᴛᴜs: ᴏɴʟɪɴᴇ ✨**\n\n"
+                    response += f"🟢 [{bot.first_name}](tg://user?id={bot.id}) - **sᴛᴀᴛᴜs: ᴏɴʟɪɴᴇ ✨**\n\n"
                 else:
-                    response += f"╭⎋ [{bot.first_name}](tg://user?id={bot.id})\n╰⊚ **sᴛᴀᴛᴜs: ᴏғғʟɪɴᴇ ❄**\n\n"
-        except Exception:
-            response += f"╭⎋ {bot_username}\n╰⊚ **sᴛᴀᴛᴜs: ᴇʀʀᴏʀ ❌**\n"
+                    response += f"🔴 [{bot.first_name}](tg://user?id={bot.id}) - **sᴛᴀᴛᴜs: ᴏғғʟɪɴᴇ ❄**\n\n"
+        except Exception as e:
+            response += f"❌ {bot_username} - **sᴛᴀᴛᴜs: ᴇʀʀᴏʀ ({str(e)})**\n\n"
     
     await msg.edit_text(response)
 
-
-
-
-
 @app.on_message(filters.command("addbot") & filters.user(OWNER_ID))
-async def add_bot(app, message):
+async def add_bot(_, message):
     bruh = message.text.split(maxsplit=1)[1]
-    data = bruh.split(" ")
-    add_bots = list(data)
-    response = "successfully added bots in bots checker list\n\n"
-    msg = await message.reply("wait sir...")
+    add_bots = bruh.split()
+    response = "Successfully added bots to the bot checker list\n\n"
+    msg = await message.reply("Please wait...")
     
     for i in add_bots:
         if i not in BOT_LIST:
             BOT_LIST.append(i)
-            response += f"{i} Added .\n\n"
+            response += f"{i} Added.\n\n"
         else:
-            response += f"{i} Already\n\n"
+            response += f"{i} Already added.\n\n"
     await msg.edit_text(response)
 
-
-
 @app.on_message(filters.command("rmbot") & filters.user(OWNER_ID))
-async def remove_bot(app, message):
+async def remove_bot(_, message):
     bruh = message.text.split(maxsplit=1)[1]
-    data = bruh.split(" ")
-    remove_bots = list(data)
-    response = "successfully removed bots on the bots list\n\n"
-    msg = await message.reply("wait baby...")
+    remove_bots = bruh.split()
+    response = "Successfully removed bots from the bot list\n\n"
+    msg = await message.reply("Removing bots...")
     
     for i in remove_bots:
         if i in BOT_LIST:
             BOT_LIST.remove(i)
             response += f"{i} Removed.\n\n"
         else:
-            response += f"{i} Not in list.\n\n"
+            response += f"{i} Not found in the list.\n\n"
 
     await msg.edit_text(response)
-
-
