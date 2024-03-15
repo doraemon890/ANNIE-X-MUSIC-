4f1d4c06bcd1b06e5bc6be98edc1154f6fe38eb3
RAJNISH XD <155803358+doraemon890@users.noreply.github.com>
2024-03-15 09:28:58 +0530

    Update mention.py

--- ANNIEMUSIC/plugins/admins/mention.py
+++ ANNIEMUSIC/plugins/admins/mention.py
@@ -1,22 +1,19 @@
 import asyncio
-import random
-from pyrogram import Client, filters
 from pyrogram.enums import ChatType, ChatMemberStatus
-from pyrogram.errors import UserNotParticipant
-from pyrogram.types import ChatPermissions
 from ANNIEMUSIC import app
-from ANNIEMUSIC.utils.jarvis_ban import admin_filter
+from pyrogram import filters
+from ANNIEMUSIC.utils.admin_check import admin_filter
+
 
 
 SPAM_CHATS = []
 
 
-@app.on_message(filters.command(["all", "mention", "mentionall"], prefixes=["/", "@", "."]) & admin_filter)
+@app.on_message(filters.command(["utag", "ull"]) & filters.group & admin_filter)
 async def tag_all_users(_,message): 
-
     replied = message.reply_to_message  
     if len(message.command) < 2 and not replied:
-        await message.reply_text("** ɢɪᴠᴇ sᴏᴍᴇ ᴛᴇxᴛ ᴛᴏ ᴛᴀɢ ᴀʟʟ, ʟɪᴋᴇ »** `@all Hey How Are You!`") 
+        await message.reply_text("**ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴍᴇssᴀɢᴇ ᴏʀ ɢɪᴠᴇ sᴏᴍᴇ ᴛᴇxᴛ ᴛᴏ ᴛᴀɢ ᴀʟʟ**") 
         return                  
     if replied:
         SPAM_CHATS.append(message.chat.id)      
@@ -26,7 +23,7 @@ async def tag_all_users(_,message):
             if message.chat.id not in SPAM_CHATS:
                 break       
             usernum += 5
-            usertxt += f"\n⊚ [{m.user.first_name}](tg://user?id={m.user.id})\n"
+            usertxt += f" [{m.user.first_name}](tg://user?id={m.user.id})"
             if usernum == 1:
                 await replied.reply_text(usertxt)
                 await asyncio.sleep(2)
@@ -46,9 +43,9 @@ async def tag_all_users(_,message):
             if message.chat.id not in SPAM_CHATS:
                 break 
             usernum += 1
-            usertxt += f"\n⊚ [{m.user.first_name}](tg://user?id={m.user.id})\n"
+            usertxt += f" [{m.user.first_name}](tg://user?id={m.user.id})"
             if usernum == 5:
-                await app.send_message(message.chat.id,f'{text}\n{usertxt}\n\n|| ➥ ᴏғғ ᴛᴀɢɢɪɴɢ ʙʏ » /cancel ||')
+                await app.send_message(message.chat.id,f'{text}\n{usertxt}')
                 await asyncio.sleep(2)
                 usernum = 0
                 usertxt = ""                          
@@ -57,7 +54,7 @@ async def tag_all_users(_,message):
         except Exception:
             pass        
            
-@app.on_message(filters.command(["stopmention", "offall", "cancel", "allstop", "stopall", "cancelmention", "offmention", "mentionoff", "alloff", "cancelall", "allcancel" ], prefixes=["/", "@", "."]) & admin_filter)
+@app.on_message(filters.command("cancel") & ~filters.private)
 async def cancelcmd(_, message):
     chat_id = message.chat.id
     if chat_id in SPAM_CHATS:
@@ -65,7 +62,7 @@ async def cancelcmd(_, message):
             SPAM_CHATS.remove(chat_id)
         except Exception:
             pass   
-        return await message.reply_text("**ᴛᴀɢɢɪɴɢ ᴘʀᴏᴄᴇss sᴜᴄᴄᴇssғᴜʟʟʏ sᴛᴏᴘᴘᴇᴅ!**")     
+        return await message.reply_text("**ᴛᴀɢ ᴀʟʟ sᴜᴄᴄᴇssғᴜʟʟʏ sᴛᴏᴘᴘᴇᴅ!**")     
                                      
     else :
         await message.reply_text("**ɴᴏ ᴘʀᴏᴄᴇss ᴏɴɢᴏɪɴɢ!**")  
