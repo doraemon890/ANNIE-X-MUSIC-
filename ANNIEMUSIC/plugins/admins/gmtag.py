b0cfe03124b4be5d4dc90e989bc311173248da53
RAJNISH XD <155803358+doraemon890@users.noreply.github.com>
2024-03-17 22:57:39 +0530

    Update gmtag.py

--- ANNIEMUSIC/plugins/admins/gmtag.py
+++ ANNIEMUSIC/plugins/admins/gmtag.py
@@ -207,7 +207,7 @@ async def mention_allvc(client, message):
 
 
 
-@app.on_message(filters.command(["gmstop", "gnstop", "cancel"]))
+@app.on_message(filters.command(["gmstop", "gnstop"]))
 async def cancel_spam(client, message):
     if not message.chat.id in spam_chats:
         return await message.reply("๏ ᴄᴜʀʀᴇɴᴛʟʏ ɪ'ᴍ ɴᴏᴛ ᴛᴀɢɢɪɴɢ ʙᴀʙʏ.")
