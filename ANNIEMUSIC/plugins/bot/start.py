063594dd7640f480d4764ccb4c09072c033123da
RAJNISH XD <155803358+doraemon890@users.noreply.github.com>
2024-03-23 13:54:04 +0530

    Update start.py

--- ANNIEMUSIC/plugins/bot/start.py
+++ ANNIEMUSIC/plugins/bot/start.py
@@ -41,7 +41,13 @@ ANNIE_VID = [
 "https://telegra.ph/file/943bb99829ec526c3f99a.mp4"
 
 ]
+# Random stickers
 
+STICKER = [
+    "CAACAgUAAx0CfL_LsAACCSxl_ov89WkiooOodUBTfSc_AAHlFk4AAhgHAALJdnBVsMGurYbGRK4eBA",
+    "CAACAgUAAx0CfL_LsAACCSRl_oru7uW8WAt3-L1pYQWe_1mxawACQw8AAj78MVeb3v2OFvEnNB4E",
+    "CAACAgUAAx0CfL_LsAACCSBl_orYW5vSoMibMlfCM1iKf_abrQACLAgAAh1raVU100jZAAEe5VUeBA",
+]
 
 
 
@@ -53,6 +59,9 @@ async def start_pm(client, message: Message, _):
         name = message.text.split(None, 1)[1]
         if name[0:4] == "help":
             keyboard = help_pannel(_)
+            umm = await m.reply_sticker(sticker=random.choice(STICKER))
+            await asyncio.sleep(2)
+            await umm.delete()
             return await message.reply_video(
                 random.choice(ANNIE_VID),
                 caption=_["help_1"].format(config.SUPPORT_CHAT),
@@ -105,6 +114,9 @@ async def start_pm(client, message: Message, _):
                 )
     else:
         out = private_panel(_)
+        umm = await m.reply_sticker(sticker=random.choice(STICKER))
+        await asyncio.sleep(2)
+        await umm.delete()  
         served_chats = len(await get_served_chats())
         served_users = len(await get_served_users())
         UP, CPU, RAM, DISK = await bot_sys_stats()
